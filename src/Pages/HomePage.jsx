baccbdaf50352728571ce4251cf62c4ea7b167a6
Shivam24S <shivamshrivastav8055@gmail.com>
2024-03-13 18:40:16 +0530

    project Complete

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import PageContent from "../components/ErrorPageContent";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <PageContent title="Welcome">
+        <h1>Browser Amazing Events</h1>
+      </PageContent>
+    </>
+  );
 };
 
 export default HomePage;
